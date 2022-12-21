3aa64b0d172638b705f1894ab26dfdb274d22bbf
TAI-TUNG-EN <chloe981219@gmail.com>
2022-12-22 00:43:55 +0900

    fix the line break problem of login page in mobile view

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {  signInWithEmailAndPassword   } from 'firebase/auth';
 import { auth } from './firebase';
 import { NavLink, useNavigate } from 'react-router-dom'
@@ -34,10 +34,31 @@ const Login = () => {
           console.log(errorCode, errorMessage)
       });
   }
+
+  const [width, setWidth] = useState(window.innerWidth);
+  function handleWindowSizeChange() {
+      setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+  const isMobile = width <= 768;
+  const isPad = (width > 768) && (width <= 992);
+
+  var titleContents = ["Hello, welcome to ","Tai, Tung-En’s portfolio."]
+  if(isMobile){
+    var titleRender = titleContents.map(item => <div>{item}</div>)
+  } else {
+    var titleRender = titleContents[0] + titleContents[1]
+  }
+
   return(
     <div id={loginCss.login}>
       <div className={loginCss.login_container}>                                            
-        <p className={loginCss.login_title_prop}> Hello, welcome to Tai, Tung-En’s portfolio. </p>                       
+        <div className={loginCss.login_title_prop}> {titleRender} </div>                   
         <div className={loginCss.login_description_container}>
           <span className={loginCss.login_description_prop}>ポートフォリオをご覧いただきありがとうございます。</span>
           <span className={loginCss.login_description_prop}>事前に提示されたメールアドレスとパスワードをご記入ください。</span>
