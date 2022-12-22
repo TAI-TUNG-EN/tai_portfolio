8fd3f71f43ea6cf51d679d656db43d3cebb98aa1
TAI-TUNG-EN <chloe981219@gmail.com>
2022-12-23 01:11:25 +0900

    fix the animation duration and delay

--- src/About.js
+++ src/About.js
@@ -555,7 +555,14 @@ export default function About() {
 
           <div className={aboutCss.about_learn_me_by_hashtags_button_container} ref={learn_me_by_hashtag_ref}>
             <div className={aboutCss.about_learn_me_by_hashtags_button_prop_container}>
-              <Link to="/hashtags" className={aboutCss.about_learn_me_by_hashtags_button_link}>
+              <Link to="/hashtags" className={aboutCss.about_learn_me_by_hashtags_button_link}
+                data-aos="fade-up"
+                data-aos-easing="ease-in-out"
+                data-aos-delay="1100"
+                data-aos-offset="10"
+                data-aos-duration="500"
+                data-aos-once="false"
+                data-aos-anchor-placement="top-bottom">
                 <div className={aboutCss.about_learn_me_by_hashtags_contents}>
                   Learn me by #️⃣hashtags
                 </div>
